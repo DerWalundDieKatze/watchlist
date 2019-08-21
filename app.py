14c71c54ddf66ae15998839f799557d4b527f0ef
caroline <caroline.fang.cc@gmail.com>
2019-08-21 14:16:00 +0800

    Add home page

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)  # 实例引入的flask对象
 
 
-@app.route('/')             # 使用url装饰器绑定url
+@app.route('/')  # 使用url装饰器绑定url
+@app.route('/home')
+@app.route('/index')
 def hello():
-    return 'Welcome to My Watchlist!'
+    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+
+
+@app.route('/user/<name>')
+def user_page(name):
+    return 'User: %s' % name
+
+
+@app.route('/test')
+def test_url_for():
+    # 下面是一些调用示例（请在命令行窗口查看输出的 URL）：
+    print(url_for('hello'))  # 输出：/
+    # 注意下面两个调用是如何生成包含 URL 变量的 URL 的
+    print(url_for('user_page', name='name'))  # 输出：/user/name
+    print(url_for('user_page', name='peter'))  # 输出：/user/peter
+    print(url_for('test_url_for'))  # 输出：/test
+    # 下面这个调用传入了多余的关键字参数，它们会被作为查询字符串附加到 URL 后面。
+    print(url_for('test_url_for', num=2))  # 输出：/test?num=2
+    return 'Test page'
